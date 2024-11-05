2d5cc6032c7c6e0294e0da6791eebe794677d970
Md Imon Hossain <imoncoc0@gmail.com>
2024-11-05 11:35:03 +0600

    edit added

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
-const inputBox = document.getElementById("input-box")
-const listContainer = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
 function addTask() {
     if(inputBox.value === ''){
-        alert("You must write something!")
+        alert("You must write something!");
     }
     else{
-        let li = document.createElement("li")
+        let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7";
-        li.appendChild(span)
+
+        let editSpan = document.createElement("span");
+        editSpan.innerHTML = "✏️";
+        editSpan.className = "edit-span";
+        editSpan.addEventListener('click', editTask);
+        li.appendChild(editSpan);
+
+        let deleteSpan = document.createElement("span");
+        deleteSpan.innerHTML = "\u00d7";
+        deleteSpan.className = "delete-span";
+        deleteSpan.addEventListener('click', deleteTask);
+        li.appendChild(deleteSpan);
     }
 
     inputBox.value = "";
     saveData();
 }
 
+listContainer.addEventListener("click", function(e) { if (e.target.tagName === "LI") { e.target.classList.toggle("checked"); saveData(); } }, false);
 
-listContainer.addEventListener("click", function(e) {
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked")
-        saveData();
+function editTask(event) {
+    const li = event.target.parentElement;
+    const newValue = prompt("Edit your task:", li.childNodes[0].nodeValue);
+    if (newValue !== null) {
+        li.childNodes[0].nodeValue = newValue;
     }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false)
+    saveData();
+}
+
+function deleteTask(event) {
+    event.target.parentElement.remove();
+    saveData();
+}
 
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML)
-}
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data")
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
-showTask()
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data") || '';
+}
+
+showTask();
